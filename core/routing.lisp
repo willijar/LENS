3317f69943d281367e4fa22ee4f0aba40fac1c76
John Williams <git@jarw.org.uk>
2010-12-06 20:25:45 +0000

    Added in routing interface

--- core/routing.lisp
+++ core/routing.lisp
@@ -6,34 +6,44 @@
 
 (in-package :layer3)
 
-(defstruct next-hop
-  interface address)
+;; vertex for topology and routing
+;; we store interface entities for efficiency although ipaddresses are used by
+;; protocols
+(defstruct vertex start end)
+(defun vertex=(a b)
+  (and (eql (vertex-start a) (vertex-start b))
+       (eql (vertex-end a) (vertex-end b))))
 
 (defclass routing()
   ((node :type node :initarg :node :reader node
-         :documentation "Nope to which routing object is attached")
-   (default-route :type next-hop :initform nil :accessor default-route
+         :documentation "Node to which routing object is attached")
+   (default-route :type vertex :initform nil :accessor default-route
                   :documentation "The default (gateway) route"))
   (:documentation "Base class for all the routing protocols that may
 be needed for a simulation."))
 
-(defgeneric add-route(dest mask interface nexthop routing)
+(defgeneric getroute(network-address routing &key packet &allow-other-keys)
+  (:documentation "Lookup vertex routing entry from node to ipaddr (possibly
+using source routing in packet")
+  (:method(any routing &key &allow-other-keys)
+    (declare (ignore any))
+    (default-route routing)))
+
+(defgeneric (setf getroute)(vertex network-address routing &key &allow-other-keys)
   (:documentation "Add a routing entry to dest ip address using subnet mask and
 interface and next hop IP address"))
 
-(defgeneric rem-route(dest mask routing)
+(defgeneric remroute(network-address routing &key &allow-other-keys)
   (:documentation "Remove a route to dest using subnet"))
 
-(defgeneric find-route(ipaddr routing &optional packet)
-  (:documentation "Lookup routing-entry from node to ipaddr (possibly
-using source routing in packet"))
-
-(defgeneric initialise-routes(routing)
-  (:documentation "Initialise routing table"))
+(defun leaf-node-p(node)
+  (with-slots(interfaces) node
+    (and (= 1 (length interfaces))
+         (= 1 (length (peer-interfaces (link (aref interfaces 0))))))))
 
 (defgeneric reinitialise-routes(routing changed-entity)
   (:documentation "Reinitialise routing table due to topology change -
-changed-entity is the object in the topology who's state has changed"))
+changed-entity is the object in the topology who's state has changed. If no changed entity specified initialise entire state"))
 
 (defgeneric topology-changed(changed-entity)
   (:documentation "Inform routing that topology has changed")
@@ -42,6 +52,8 @@ changed-entity is the object in the topology who's state has changed"))
 
 (defvar *default-routing* nil "make-instance args for default routing")
 
+
+
 (defun routing-neighbours(node &key no-leaf)
   "Return list of routing neighours for a node - if no-leaf is true do
  not include leaf nodes in the list. Only uses up interfaces and
@@ -49,9 +61,14 @@ changed-entity is the object in the topology who's state has changed"))
   (when (up-p node)
     (mapcan
      #'(lambda(interface)
-         (let ((peers (filter #'up-p (peer-interfaces interface))))
-           (unless (and no-lead (<= (length peers) 1))
+         (let ((peers (filter #'up-p (peer-interfaces (link interface) interface))))
+           (unless (and no-leaf (<= (length peers) 1))
              (mapcar
-              #'(lambda(peer) (make-next-hop interface (network-address peer)))
+              #'(lambda(peer) (make-vertex :start interface :end peer))
               peers))))
      (filter #'up-p (interfaces node)))))
+
+(defmethod find-route((addr network-address) routing &optional packet)
+  (let ((node (find addr (node:nodes) :key #'network-address)))
+    (unless node (error "Node with network address ~A not found" addr))
+    (find-route node routing packet)))
