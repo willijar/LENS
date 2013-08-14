5e177c4b45dd4c98546fa85eb0b48ab7e7144447
John Williams <git@jarw.org.uk>
2013-08-14 16:02:52 +0100

    Further fix on deployment/location

--- networks/wsn/core/mobility.lisp
+++ networks/wsn/core/mobility.lisp
@@ -23,6 +23,11 @@
    (static-p :initform t :initarg :static-p :reader static-p))
   (:metaclass module-class))
 
+(defmethod initialize-instance :after ((instance mobility)
+                                       &key &allow-other-keys)
+  (unless (slot-boundp instance 'location)
+    (parse-deployment instance)))
+
 (defgeneric (setf location)(location instance)
   (:documentation "Change location in mobility manager")
   (:method((location coord) (instance mobility))
@@ -38,3 +43,49 @@
   (case stage
     (0  (tracelog "initial location is ~A" (location instance))))
   t)
+
+(defun parse-deployment(instance)
+  (let* ((node (node instance))
+         (network (network node))
+         (xlen (coord-x (field network)))
+         (ylen (coord-y (field network)))
+         (zlen (coord-z (field network))))
+    (multiple-value-bind(deployment start-index)
+        (let ((deployment (deployment network)))
+          (if (range-list-p deployment)
+              (range-getf (deployment network) (index node))
+              (values deployment 0)))
+      (when deployment
+        (setf
+         (slot-value instance 'location)
+         (cond
+           ((eql deployment 'uniform)
+            (make-coord (uniform 0 xlen) (uniform 0 ylen) (uniform 0 zlen)))
+           ((eql deployment 'center)
+            (make-coord (/ xlen 2) (/ ylen 2) (/ zlen 2)))
+           ((and (listp deployment)
+                 (member (first deployment) '(grid randomized)))
+            (let* ((gridi (- (index node) start-index))
+                   (gridx (second deployment))
+                   (gridy (third deployment))
+                   (gridz (fourth deployment))
+                   (grid
+                    (make-coord
+                     (* (mod gridi gridx) (/ xlen (1- gridx)))
+                     (* (mod (floor gridi gridx) gridy) (/ ylen (1- gridy)))
+                     (if (and gridz zlen)
+                         (* (mod (floor gridi (* gridx gridy)) gridz)
+                            (/ zlen (1- gridz)))
+                         0.0))))
+              (if (eql (first deployment) 'randomized)
+                  (flet((rn(len grid)
+                          (let ((s (/ len grid)))
+                            (min (- s) (max s (normal 0 (* 0.3 s)))))))
+                    (coord+
+                     grid
+                     (make-coord
+                      (rn xlen gridx)
+                      (rn ylen gridy)
+                      (if (and gridz zlen) (rn zlen gridz) 0.0))))
+                grid)))
+         (t (error "Unknown deployment parameter: ~A" deployment))))))))
--- networks/wsn/mobility/no-mobility.lisp
+++ networks/wsn/mobility/no-mobility.lisp
@@ -4,55 +4,5 @@
   ()
   (:metaclass module-class)
   (:default-initargs :static-p t)
-  (:documentation "Just an alias for mobility for onsistency in
+  (:documentation "Just an alias for mobility for consistency in
   parameter naming"))
-
-(defmethod initialize-instance :after ((instance no-mobility)
-                                       &key &allow-other-keys)
-  (parse-deployment instance))
-
-(defun parse-deployment(instance)
-  (let* ((node (node instance))
-         (network (network node))
-         (xlen (coord-x (field network)))
-         (ylen (coord-y (field network)))
-         (zlen (coord-z (field network))))
-    (multiple-value-bind(deployment start-index)
-        (let ((deployment (deployment network)))
-          (if (range-list-p deployment)
-              (range-getf (deployment network) (index node))
-              (values deployment 0)))
-      (when deployment
-        (setf
-         (slot-value instance 'location)
-         (cond
-           ((eql deployment 'uniform)
-            (make-coord (uniform 0 xlen) (uniform 0 ylen) (uniform 0 zlen)))
-           ((eql deployment 'center)
-            (make-coord (/ xlen 2) (/ ylen 2) (/ zlen 2)))
-           ((and (listp deployment)
-                 (member (first deployment) '(grid randomized)))
-            (let* ((gridi (- (index node) start-index))
-                   (gridx (second deployment))
-                   (gridy (third deployment))
-                   (gridz (fourth deployment))
-                   (grid
-                    (make-coord
-                     (* (mod gridi gridx) (/ xlen (1- gridx)))
-                     (* (mod (floor gridi gridx) gridy) (/ ylen (1- gridy)))
-                     (if (and gridz zlen)
-                         (* (mod (floor gridi (* gridx gridy)) gridz)
-                            (/ zlen (1- gridz)))
-                         0.0))))
-              (if (eql (first deployment) 'randomized)
-                  (flet((rn(len grid)
-                          (let ((s (/ len grid)))
-                            (min (- s) (max s (normal 0 (* 0.3 s)))))))
-                    (coord+
-                     grid
-                     (make-coord
-                      (rn xlen gridx)
-                      (rn ylen gridy)
-                      (if (and gridz zlen) (rn zlen gridz) 0.0))))
-                grid)))
-         (t (error "Unknown deployment parameter: ~A" deployment))))))))
